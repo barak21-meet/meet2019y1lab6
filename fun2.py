262e708cb0e4692bb9f0e15f0d41adb33c50cebb
barak21-meet <barak21@meet.mit.edu>
2019-07-29 10:28:40 +0300

    Stopping here

--- fun2.py
+++ fun2.py
@@ -12,16 +12,61 @@ def up():
 turtle.onkey(up, "Up")
 turtle.listen()
 
+
+def down():
+    turtle.direction="Down"
+    print("You pressed the down key")
+    on_move()
+
+turtle.onkey(down, "Down")
+turtle.listen()
+
+def left():
+    turtle.direction="Left"
+    print("You pressed the left key")
+    on_move()
+
+turtle.onkey(left, "Left")
+turtle.listen()
+
+def right():
+    turtle.direction="Right"
+    print("You pressed the right key")
+    on_move()
+
+turtle.onkey(right, "Right")
+turtle.listen()
+
+def space():
+    turtle.penup()
+
+    
+turtle.onkey(space, "space")
+turtle.listen()
+
+def c():
+    turtle.pendown()
+
+turtle.onkey(c, "c")
+turtle.listen()
+    
 def on_move():
     p=turtle.pos()
 
+
     if turtle.direction=="Up":
+        turtle.left(90)
         turtle.forward(50)
+        turtle.right(90)
 
     if turtle.direction=="Down":
+        turtle.right(90)
         turtle.forward(50)
+        turtle.left(90)
 
     if turtle.direction=="Left":
+        turtle.right(180)
         turtle.forward(50)
+        turtle.left(180)
     if turtle.direction=="Right":
         turtle.forward(50)
---
+++ fun3.py
@@ -0,0 +1,10 @@
+
+def draw_1d(n,c):
+    return(c * n)
+
+def draw_2d(m, n, c):
+
+    for d in range(m):
+        return(draw_1d(n, c))
+
+print(draw_2d(6,2,'*'))
